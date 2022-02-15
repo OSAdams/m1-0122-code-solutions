60b810c316e68bcc98ae156d8ab98b2eb3548a55
Owen Adams <owenadams.main@gmail.com>
2022-02-15 00:10:14 -0500

    assign numeric value to variables. find average of 3 variables. console log value and typeof

--- javascript-operators-and-expressions/main.js
+++ javascript-operators-and-expressions/main.js
@@ -9,3 +9,10 @@ var payment = 237;
 var change = bill - payment;
 console.log('value of change: ', change);
 console.log('typeof change: ', typeof change);
+
+var quizzes = 80;
+var midterm = 90;
+var final = 100;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade: ', grade);
+console.log('typeof grade: ', typeof grade);
