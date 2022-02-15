382e46456329560a3c253b721e420cfa9058c0d8
Owen Adams <owenadams.main@gmail.com>
2022-02-15 13:50:55 -0500

    add object literal. add two property value pairs to object. console log values

--- javascript-objects/main.js
+++ javascript-objects/main.js
@@ -5,9 +5,24 @@ var student = {
 };
 
 var fullName = student.firstName + ' ' + student.lastName;
+
 console.log('value of fullName: ', fullName);
 
 student.livesInIrvine = false;
 student.previousOccupation = 'Team Lead in Customer Service and Warehousing';
+
 console.log('value of student.livesInIrvine: ', student.livesInIrvine);
 console.log('value of student.previousOccupation: ', student.previousOccupation);
+
+var vehicle = {
+  make: 'Volkswagen',
+  model: 'Passat 2.0T LUX',
+  year: 2008
+};
+
+vehicle['color'] = 'Black';
+vehicle['isConvertible'] = false;
+
+console.log('value of vehicle["color"]: ', vehicle['color']);
+console.log('value of vehicle["isConvertible"]: ', vehicle['isConvertible']);
+console.log('value of vehicle: ', vehicle);
