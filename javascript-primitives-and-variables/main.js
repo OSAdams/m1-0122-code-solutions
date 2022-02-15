e851b5c7b191b5cfe506e80dee571474406519fc
Owen Adams <owenadams.main@gmail.com>
2022-02-14 22:32:35 -0500

    add console log for typeof

--- javascript-primitives-and-variables/main.js
+++ javascript-primitives-and-variables/main.js
@@ -7,3 +7,8 @@ const totalPets = 3;
 console.log(`variable fullName equals: ${fullName}`);
 console.log(`variable isCool equals: ${isCool}`);
 console.log(`variable totalPets equals: ${totalPets}`);
+
+// console log typeofs
+console.log(`typeof fullName equals: ${typeof fullName}`);
+console.log(`typeof isCool equals: ${typeof isCool}`);
+console.log(`typeof totalPets equals: ${typeof totalPets}`);
