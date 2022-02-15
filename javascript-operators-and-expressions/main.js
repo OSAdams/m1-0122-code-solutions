71439f91d667f848ccc344d0dc3aa6d947fee27f
Owen Adams <owenadams.main@gmail.com>
2022-02-15 00:24:07 -0500

    assign variable value to variable. add and re assign string to said variable. console log value and typeof. ready for submit

--- javascript-operators-and-expressions/main.js
+++ javascript-operators-and-expressions/main.js
@@ -32,3 +32,8 @@ var headCount = 1337;
 var isSparta = headCount === 300;
 console.log('value of isSparta: ', isSparta);
 console.log('typeof isSparta: ', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('value of motto: ', motto);
+console.log('typeof motto: ', typeof motto);
