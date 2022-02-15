77e058933d172e094354a649d5c11449c91da0c5
Owen Adams <owenadams.main@gmail.com>
2022-02-14 22:34:37 -0500

    add variable with null value. console log typeof for said variable

--- javascript-primitives-and-variables/main.js
+++ javascript-primitives-and-variables/main.js
@@ -2,6 +2,7 @@
 const fullName = 'Owen Adams';
 const isCool = true;
 const totalPets = 3;
+const unknown = null;
 
 // console log value of variables
 console.log(`variable fullName equals: ${fullName}`);
@@ -12,3 +13,4 @@ console.log(`variable totalPets equals: ${totalPets}`);
 console.log(`typeof fullName equals: ${typeof fullName}`);
 console.log(`typeof isCool equals: ${typeof isCool}`);
 console.log(`typeof totalPets equals: ${typeof totalPets}`);
+console.log(`typeof unkown equals: ${typeof unknown}`);
