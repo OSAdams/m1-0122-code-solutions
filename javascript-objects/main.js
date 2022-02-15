f51d596910a19121e0c8f7ac798ee68aa6e06eba
Owen Adams <owenadams.main@gmail.com>
2022-02-15 13:46:19 -0500

    add properties to student object. console log values

--- javascript-objects/main.js
+++ javascript-objects/main.js
@@ -6,3 +6,8 @@ var student = {
 
 var fullName = student.firstName + ' ' + student.lastName;
 console.log('value of fullName: ', fullName);
+
+student.livesInIrvine = false;
+student.previousOccupation = 'Team Lead in Customer Service and Warehousing';
+console.log('value of student.livesInIrvine: ', student.livesInIrvine);
+console.log('value of student.previousOccupation: ', student.previousOccupation);
