3087d18c1d7672d95551ccf4113bd7cb5a32e572
Owen Adams <owenadams.main@gmail.com>
2022-02-15 13:42:49 -0500

    add object literal, concat two keys of object and console log value

---
+++ javascript-objects/main.js
@@ -0,0 +1,8 @@
+var student = {
+  firstName: 'Owen',
+  lastName: 'Adams',
+  age: 30
+};
+
+var fullName = student.firstName + ' ' + student.lastName;
+console.log('value of fullName: ', fullName);
