9245ab48b80ffa1c94b030b98d77ff026646f0a5
Owen Adams <owenadams.main@gmail.com>
2022-02-15 00:07:59 -0500

    assign value to variables, console log value and typeof

--- javascript-operators-and-expressions/main.js
+++ javascript-operators-and-expressions/main.js
@@ -3,3 +3,9 @@ var height = 4;
 var area = width * height;
 console.log('value of area: ', area);
 console.log('typeof area: ', typeof area);
+
+var bill = 100000;
+var payment = 237;
+var change = bill - payment;
+console.log('value of change: ', change);
+console.log('typeof change: ', typeof change);
