99b4c81adb3908e4e6b92632027ca1e5e9571744
Owen Adams <owenadams.main@gmail.com>
2022-02-14 23:12:15 -0500

    updated argument in console log

--- javascript-primitives-and-variables/main.js
+++ javascript-primitives-and-variables/main.js
@@ -13,8 +13,8 @@ console.log('value of unknown: ', unknown);
 console.log('value of never: ', never);
 
 // console log typeofs
-console.log('typeof fullName: ', fullName);
-console.log('typeof isCool: ', isCool);
-console.log('typeof totalPets: ', totalPets);
-console.log('typeof unknown: ', unknown);
-console.log('typeof never: ', never);
+console.log('typeof fullName: ', typeof fullName);
+console.log('typeof isCool: ', typeof isCool);
+console.log('typeof totalPets: ', typeof totalPets);
+console.log('typeof unknown: ', typeof unknown);
+console.log('typeof never: ', typeof never);
