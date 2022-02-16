2f73f96ff597f3ce1f65bd7232bf3203c80a2e79
Owen Adams <owenadams.main@gmail.com>
2022-02-15 22:20:59 -0500

    add array literal. console log each index

---
+++ javascript-arrays/main.js
@@ -0,0 +1,11 @@
+// assign array literal to variable
+var colors = [
+  'red',
+  'white',
+  'blue'
+];
+
+// console log all indexes of array literal
+console.log('value of colors[0]: ', colors[0]);
+console.log('value of colors[1]: ', colors[1]);
+console.log('value of colors[2]: ', colors[2]);
