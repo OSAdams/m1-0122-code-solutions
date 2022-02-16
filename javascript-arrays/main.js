bbf889db44b3e5b920f92f7d629839314b70e8e7
Owen Adams <owenadams.main@gmail.com>
2022-02-15 22:23:20 -0500

    add new variable and console log value

--- javascript-arrays/main.js
+++ javascript-arrays/main.js
@@ -9,3 +9,9 @@ var colors = [
 console.log('value of colors[0]: ', colors[0]);
 console.log('value of colors[1]: ', colors[1]);
 console.log('value of colors[2]: ', colors[2]);
+
+// concat all indexes of colors array and assign to new variable
+var murica = 'America is ' + colors[0] + ' ' + colors[1] + ' ' + colors[2] + '.';
+
+// console log value of new variable
+console.log('value of murica: ', murica);
