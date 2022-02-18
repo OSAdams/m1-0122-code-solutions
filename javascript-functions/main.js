d8605a6782874d8fcb68119dc411d7b38330394f
Owen Adams <owenadams.main@gmail.com>
2022-02-17 22:13:28 -0500

    covert minutes to seconds complete

---
+++ javascript-functions/main.js
@@ -0,0 +1,7 @@
+function convertMinutesToSeconds(minutes) {
+  return minutes * 60;
+}
+
+var convertMinutesToSecondsResult = convertMinutesToSeconds(5);
+
+console.log('Minutes to Seconds Result:', convertMinutesToSecondsResult);
