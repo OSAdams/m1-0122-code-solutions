c9782e883b5c4211027581d04777987868af3655
Owen Adams <owenadams.main@gmail.com>
2022-02-21 22:04:28 -0500

    add variable with array literal containnig objects. use methods and console log values. array methods complete

--- javascript-methods/main.js
+++ javascript-methods/main.js
@@ -35,3 +35,65 @@ var randomHero = heroes[randomIndex];
 
 // console log randomHero
 console.log('Our current favorite super hero is:', randomHero);
+
+// Array Methods
+
+// assign 3 object literals in an array to a variable
+var library = [
+  {
+    title: 'Astrophysics for People in a Hurry',
+    author: 'Neil deGrasse Tyson'
+  },
+  {
+    title: 'Golf is Not a Game of Perfect',
+    author: 'Dr. Bob Rotella with Bob Cullen'
+  },
+  {
+    title: 'JavaScript for impatient programmers',
+    author: 'Dr. Axel Rauschmayer'
+  }
+];
+
+// console log ibrary
+console.log('value of library:', library);
+
+// use pop method on library and assign value to lastBook
+var lastBook = library.pop();
+
+// console log lastBook
+console.log('value of lastBook:', lastBook);
+
+// call shift method on library and assign value to firstBook
+var firstBook = library.shift();
+
+// console log firstBook
+console.log('value of firstBook:', firstBook);
+
+// create two new bbook objects and assign value to two seperate variables
+var js = {
+  title: 'JavaScript for Impatient Programmers',
+  author: 'Dr. Axel Rauschmayer'
+};
+var css = {
+  title: 'CSS Secrets',
+  author: 'Lea Verou'
+};
+
+// call push method of the library object with the variable js as argument
+library.push(js);
+
+// console log library
+console.log('value of library:', library);
+
+// call unshift method of library object and pass css as argument
+library.unshift(css);
+
+// console log library
+console.log('value of library:', library);
+
+// call splice method of the library object with 1, 1 as arguments
+// removing the library[1] value
+library.splice(1, 1);
+
+// console log library
+console.log('final value of library:', library);
