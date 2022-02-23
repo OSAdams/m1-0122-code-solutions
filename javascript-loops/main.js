4cbfa7ab32c0afc3698bd6b9256e3cadfc7190f2
Owen Adams <owenadams.main@gmail.com>
2022-02-22 22:36:19 -0500

    define logEachChar then test cases

--- javascript-loops/main.js
+++ javascript-loops/main.js
@@ -36,3 +36,9 @@ function repeatWord(word, times) {
   }
   return repeated;
 }
+
+function logEachCharacter(string) {
+  for (let i = 0; i < string.length; i++) {
+    console.log(string[i]);
+  }
+}
