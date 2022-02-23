7b88042af2077ccac98c811822b65b1507f2f832
Owen Adams <owenadams.main@gmail.com>
2022-02-22 22:50:16 -0500

    define doubleAll, getKeys, getValues and use test cases

--- javascript-loops/main.js
+++ javascript-loops/main.js
@@ -42,3 +42,29 @@ function logEachCharacter(string) {
     console.log(string[i]);
   }
 }
+
+function doubleAll(numbers) {
+  var doubled = [];
+  for (let i = 0; i < numbers.length; i++) {
+    doubled.push(numbers[i] * 2);
+  }
+  return doubled;
+}
+
+function getKeys(object) {
+  var keys = [];
+  for (var key in object) {
+    keys.push(key);
+  }
+  return keys;
+}
+
+function getValues(object) {
+  var values = [];
+  for (var key in object) {
+    values.push(object[key]);
+  }
+  return values;
+}
+
+// comment
