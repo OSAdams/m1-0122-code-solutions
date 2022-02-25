140cc89355f67153c680a055b644c098468ea781
Owen Adams <owenadams.main@gmail.com>
2022-02-24 23:05:21 -0500

    complete findIndex with passing test cases

--- javascript-loops-practice/find-index.js
+++ javascript-loops-practice/find-index.js
@@ -1,1 +1,12 @@
 /* exported findIndex */
+function findIndex(array, value) {
+  if (!Array.isArray(array)) {
+    return 'Error: first argument must be an array';
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      return i;
+    }
+  }
+  return -1;
+}
