7cd6afc03122f067fdad7d0235dbe8f45bed5057
Owen Adams <owenadams.main@gmail.com>
2022-02-24 23:02:39 -0500

    complete includesSeven function with passing test cases

--- javascript-loops-practice/includes-seven.js
+++ javascript-loops-practice/includes-seven.js
@@ -1,1 +1,12 @@
 /* exported includesSeven */
+function includesSeven(array) {
+  if (!Array.isArray(array)) {
+    return 'Error: argument must be an array';
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 7) {
+      return true;
+    }
+  }
+  return false;
+}
