b7b7dd53fa786aa964ed0bb35d198853cd8a7f8f
Owen Adams <owenadams.main@gmail.com>
2022-02-24 22:52:43 -0500

    complete filterOutNulls with passing test cases

--- javascript-loops-practice/filter-out-nulls.js
+++ javascript-loops-practice/filter-out-nulls.js
@@ -1,1 +1,13 @@
 /* exported filterOutNulls */
+function filterOutNulls(values) {
+  if (!Array.isArray(values)) {
+    return 'Error: argument must be an array';
+  }
+  const resultArr = [];
+  for (let i = 0; i < values.length; i++) {
+    if (values[i] !== null) {
+      resultArr.push(values[i]);
+    }
+  }
+  return resultArr;
+}
