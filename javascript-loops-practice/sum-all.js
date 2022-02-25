15b7dc5bfc2a07d896179e7055e17ac9ae4f4fdb
Owen Adams <owenadams.main@gmail.com>
2022-02-24 22:24:48 -0500

    complete sumAll function with passing test cases

--- javascript-loops-practice/sum-all.js
+++ javascript-loops-practice/sum-all.js
@@ -1,1 +1,12 @@
 /* exported sumAll */
+
+function sumAll(numbers) {
+  let resultNum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (typeof numbers[i] !== 'number') {
+      return 'Error: Values inside of the array must be an integer';
+    }
+    resultNum += numbers[i];
+  }
+  return resultNum;
+}
