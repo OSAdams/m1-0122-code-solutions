53cb6704634d74beea1c908e9ba2dc061283900c
Owen Adams <owenadams.main@gmail.com>
2022-02-24 22:48:46 -0500

    complete getStudnetNames function with passing test cases

--- javascript-loops-practice/get-student-names.js
+++ javascript-loops-practice/get-student-names.js
@@ -1,1 +1,14 @@
 /* exported getStudentNames */
+function getStudentNames(students) {
+  if (!Array.isArray(students)) {
+    return 'Error: argument must be an array';
+  }
+  const resultArr = [];
+  for (let i = 0; i < students.length; i++) {
+    if (typeof students[i] !== 'object') {
+      return 'Error: array values must be objects';
+    }
+    resultArr.push(students[i].name);
+  }
+  return resultArr;
+}
