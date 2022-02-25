26be06cae25fd62b46b236294a3246e07f61e185
Owen Adams <owenadams.main@gmail.com>
2022-02-24 22:19:12 -0500

    complete countdown function definition with passing test cases

--- javascript-loops-practice/countdown.js
+++ javascript-loops-practice/countdown.js
@@ -1,1 +1,13 @@
 /* exported countdown */
+
+function countdown(number) {
+  if (typeof number !== 'number' || number < 0) {
+    return 'Please pass a positive integer as an argument';
+  }
+  const cdResult = [];
+  while (number >= 0) {
+    cdResult.push(number);
+    number--;
+  }
+  return cdResult;
+}
