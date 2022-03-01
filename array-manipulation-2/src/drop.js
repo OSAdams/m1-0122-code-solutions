b081c1e926d7a40cd159fef9afca6e9b5d20814c
Owen Adams <owenadams.main@gmail.com>
2022-03-01 17:04:50 -0500

    complete drop function definition. passing test cases

--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -1,1 +1,22 @@
 /* exported drop */
+/*
+  define a function with two para, array and count
+  check passed arguments, if they do not match an array and number --
+  -- return error
+  check length of array
+  if not > 0, return empty array
+  assign empty array to result var
+  initialize loop
+  WIP
+*/
+function drop(array, count) {
+  // use expression to check arguments passed
+  if (!Array.isArray(array) || typeof count !== 'number') {
+    // return error if incorrect values are passed
+    return { error: 'Invalid arugments passed. First argument must be an array, second argument must be a number' };
+  }
+  // return array.slice(count)
+  return array.slice(count);
+}
+
+// ..... if only we could remember all methods LOL
