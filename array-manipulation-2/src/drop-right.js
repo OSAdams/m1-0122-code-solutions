bef1a39d77b08b1a4046de4411aba64a0ebd8474
Owen Adams <owenadams.main@gmail.com>
2022-03-01 17:15:08 -0500

    complete dropRight function definition with passing test cases

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,1 +1,10 @@
 /* exported dropRight */
+function dropRight(array, count) {
+  const result = [];
+  let i = 0;
+  while (i < array.length - count) {
+    result.push(array[i]);
+    i++;
+  }
+  return result;
+}
