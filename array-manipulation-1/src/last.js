16980cbd944989e73c40a333ad146e30202121e3
Owen Adams <owenadams.main@gmail.com>
2022-02-28 21:59:53 -0500

    complete last function definition. passing test cases

--- array-manipulation-1/src/last.js
+++ array-manipulation-1/src/last.js
@@ -1,1 +1,9 @@
 /* exported last */
+function last(array) {
+  // return undefined for an empty array
+  if (array === []) {
+    return undefined;
+  }
+  // return last index value of array
+  return array[array.length - 1];
+}
