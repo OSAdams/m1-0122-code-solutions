5b1f93a443ed3b8867255ae0a42ba123a9fb3b39
Owen Adams <owenadams.main@gmail.com>
2022-02-28 22:57:57 -0500

    complete isVowel definition. passing test cases

--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,1 +1,11 @@
 /* exported isVowel */
+function isVowel(char) {
+  const charLower = char.toLowerCase();
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  for (let i = 0; i < vowels.length; i++) {
+    if (charLower === vowels[i]) {
+      return true;
+    }
+  }
+  return false;
+}
