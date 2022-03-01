5529591349f856069c62f31afcf00242f9aac124
Owen Adams <owenadams.main@gmail.com>
2022-03-01 17:21:53 -0500

    complete includes function definition with passing test cases

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,5 +1,8 @@
 /* exported dropRight */
 function dropRight(array, count) {
+  if (!Array.isArray(array) || typeof count !== 'number') {
+    return { error: 'Invalid arguments passed. First argument must be an array, second argument must be a number' };
+  }
   const result = [];
   let i = 0;
   while (i < array.length - count) {
