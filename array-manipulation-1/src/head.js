fb1febdc08c947af55d64dc8ad741e48bebd0292
Owen Adams <owenadams.main@gmail.com>
2022-02-28 21:58:20 -0500

    completed head function defintion. passing test cases

--- array-manipulation-1/src/head.js
+++ array-manipulation-1/src/head.js
@@ -1,1 +1,9 @@
 /* exported head */
+function head(array) {
+  // return undefined if argument passed is an emtpy array
+  if (array === []) {
+    return undefined;
+  }
+  // return first index of array passed as an argument
+  return array[0];
+}
