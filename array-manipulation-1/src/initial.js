946db6d15e3a124178d2fb25e6a13caa0d33747a
Owen Adams <owenadams.main@gmail.com>
2022-02-28 22:09:43 -0500

    complete initial definition function. passing test cases

--- array-manipulation-1/src/initial.js
+++ array-manipulation-1/src/initial.js
@@ -1,1 +1,25 @@
 /* exported initial */
+
+/*
+  define a function
+  assign empty array to variable
+  return empty array if argument passed is an empty array\
+  initialize a for loop that will iterate all indexes but the last in argument passed
+  in the loop, push all values (besides the last) in the array argument passed to empty array
+  return said array
+*/
+function initial(array) {
+  // assign empty array to variable
+  const result = [];
+  // if array is empty, return result
+  if (array === []) {
+    return result;
+  }
+  // initialize a for loop that will not iterate to the last index value
+  for (let i = 0; i < array.length - 1; i++) {
+    // push index values into result array
+    result.push(array[i]);
+  }
+  // return result array
+  return result;
+}
