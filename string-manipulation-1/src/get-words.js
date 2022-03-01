7bdfcedddc6666f0097ff7b5523951ddbf760a03
Owen Adams <owenadams.main@gmail.com>
2022-02-28 23:03:45 -0500

    complete getWords definition. passing test cases

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,1 +1,9 @@
 /* exported getWords */
+function getWords(string) {
+  if (string === '') {
+    return [];
+  }
+  return string.split(' ');
+}
+
+// do not return string.split('') for an empty array return - uses more resources bad bad
