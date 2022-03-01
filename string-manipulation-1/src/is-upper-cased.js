c80d8c0f621ea4230ca7e92f157dc4571ad0af37
Owen Adams <owenadams.main@gmail.com>
2022-02-28 22:54:55 -0500

    complete isUpperCased definition. passing test cases

--- string-manipulation-1/src/is-upper-cased.js
+++ string-manipulation-1/src/is-upper-cased.js
@@ -1,1 +1,12 @@
 /* exported isUpperCased */
+/*
+  define function wiht one paramater
+  assign parameter.toUpperCase() to a variable
+  return comparison expression
+*/
+function isUpperCased(word) {
+  // assign parameter.toUpperCase() to a variable
+  const wordUpC = word.toUpperCase();
+  // return comparison expression
+  return wordUpC === word;
+}
