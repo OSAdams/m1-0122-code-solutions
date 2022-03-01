4ca2e719cdc75f9e7cf358b2a2ebbceba2848c7a
Owen Adams <owenadams.main@gmail.com>
2022-03-01 17:12:21 -0500

    complete drop function definition with passing test cases

--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,1 +1,22 @@
 /* exported takeRight */
+/*
+  define a function with two parameters
+  return error if argument values are invalid
+  use expression to return empty array if an empty array is passed as arg
+  assign expression array.length - (1 + count) to var
+  return array.slice(var)
+*/
+function takeRight(array, count) {
+  // use expression to see if arguments are valid values
+  if (!Array.isArray(array) || !typeof count === 'number') {
+    // return error with descriptive message
+    return 'Error: Invalid arugments passed. First argument must be an array, second argument must be a number';
+  }
+  // use expression to check if array.length < count
+  if (array.length < count) {
+    // return array
+    return array;
+  }
+  // return array.slice(array.length - count);
+  return array.slice(array.length - count);
+}
