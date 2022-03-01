8689fff38b5cf43a88b2238717614c99843ad68c
Owen Adams <owenadams.main@gmail.com>
2022-03-01 17:41:38 -0500

    updated chunk function with error check

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,10 +1,11 @@
 /* exported chunk */
 function chunk(array, size) {
+  if (!Array.isArray(array) || typeof size !== 'number') {
+    return { error: 'Invalid arguments passed. First argument must be an array, second argument must be a number' };
+  }
   const result = [];
   for (let i = 0; i < array.length; i += size) {
     result.push(array.slice(i, i + size));
   }
   return result;
 }
-
-// comment o remove
