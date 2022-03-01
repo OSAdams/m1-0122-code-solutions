a63f86a5bbe7bf5432aeab4f89819aec0e22c743
Owen Adams <owenadams.main@gmail.com>
2022-02-28 22:04:19 -0500

    complete tail function definition. passing test cases

--- array-manipulation-1/src/tail.js
+++ array-manipulation-1/src/tail.js
@@ -1,1 +1,16 @@
 /* exported tail */
+function tail(array) {
+  // assign an empty array to a variable
+  const result = [];
+  // return empty array if argument is an empty array
+  if (array === []) {
+    return result;
+  }
+  // initialize a for loop, iterate through the array starting at 2nd index
+  for (let i = 1; i < array.length; i++) {
+    // push index values into result array
+    result.push(array[i]);
+  }
+  // return result array of all index values other than the first of the argument passed
+  return result;
+}
