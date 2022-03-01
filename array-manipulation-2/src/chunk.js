6ef324ef1f47d56f254883fa1c31d1c1bb534c58
Owen Adams <owenadams.main@gmail.com>
2022-03-01 17:39:35 -0500

    complete chunk function definition with passing test cases

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,10 @@
 /* exported chunk */
+function chunk(array, size) {
+  const result = [];
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, i + size));
+  }
+  return result;
+}
+
+// comment o remove
