465447e488b67dffb0b53b24417f37536bbaf33d
Owen Adams <owenadams.main@gmail.com>
2022-02-28 22:51:32 -0500

    complete isLowerCased definition. passing test cases

--- string-manipulation-1/src/is-lower-cased.js
+++ string-manipulation-1/src/is-lower-cased.js
@@ -1,1 +1,12 @@
 /* exported isLowerCased */
+/*
+  define function with one parameter
+  assign parameter.toLowerCase() to a variable
+  return expression
+*/
+function isLowerCased(word) {
+  // assign parameter.toLowerCase() to a vairable
+  const wordLowerC = word.toLowerCase();
+  // return comparison expression
+  return wordLowerC === word;
+}
