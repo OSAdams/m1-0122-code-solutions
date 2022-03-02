b618d5cf64a226aed7dcfd6482827c1df3f3cf10
Owen Adams <owenadams.main@gmail.com>
2022-03-01 23:12:07 -0500

    complete string function definition with passing test cases

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,14 @@
 /* exported numVowels */
+function numVowels(string) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const lowerString = string.toLowerCase();
+  let result = 0;
+  for (let i = 0; i < lowerString.length; i++) {
+    for (const k in vowels) {
+      if (lowerString[i] === vowels[k]) {
+        result++;
+      }
+    }
+  }
+  return result;
+}
