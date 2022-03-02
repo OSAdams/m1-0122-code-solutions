c6ed2f7b9416f55b719214fb19259342addc624b
Owen Adams <owenadams.main@gmail.com>
2022-03-01 23:21:20 -0500

    complete lastChars function definition with passing test cases

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,7 @@
 /* exported lastChars */
+function lastChars(length, string) {
+  if (!parseInt(length) || typeof string !== 'string') {
+    return { error: 'Invalid arguments passed. First argument must be a number, second argument must be a string' };
+  }
+  return string.slice(-1 * length);
+}
