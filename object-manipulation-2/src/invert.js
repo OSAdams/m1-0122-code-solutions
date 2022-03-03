abf4ac01158f653fb902a432ea4f42fcdafb30b3
Owen Adams <owenadams.main@gmail.com>
2022-03-02 19:51:23 -0500

    complete invert function definition with passing test cases

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,11 @@
 /* exported invert */
+function invert(source) {
+  if (typeof source !== 'object' || Array.isArray(source)) {
+    return { error: 'Invalid argument passed. Argument must be an object' };
+  }
+  const result = {};
+  for (const i in source) {
+    result[source[i]] = i;
+  }
+  return result;
+}
