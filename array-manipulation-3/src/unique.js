4ac57ab0da54a9a79ec9a4273a1f35d75fc8f802
Owen Adams <owenadams.main@gmail.com>
2022-03-04 16:36:00 -0500

    complete unique function definition with passing test cases

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -12,5 +12,27 @@
  */
 
 function unique(array) {
-  return 'lel';
+  // assign object literal to err variable
+  const err = { error: 'Invalid argument passed. Argument must be an array' };
+  /*
+    trial an error, there is a much more complex filter method
+    im going to try something else
+    return [...new Set(array)]; <-- this works v sleak
+  */
+  // assign empty array to variable named result
+  const result = [];
+  // initialize a for... in loop for array
+  for (const i in array) {
+    // expression to check if result includes value in array[i]
+    // if not, push value into result array
+    if (!result.includes(array[i])) { result.push(array[i]); }
+  }
+  /*
+    expression to check if the argument passed is an array
+    if it isn't an array, return err variable value
+    if it is an array, return result
+  */
+  return Array.isArray(array) ? result : err;
 }
+
+// return [...new Set(array)] is quite possibly the laziest, most beautiful thing i've seen
