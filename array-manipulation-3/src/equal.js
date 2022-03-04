20f84f0e325d377b58379c057dea205c974d7f71
Owen Adams <owenadams.main@gmail.com>
2022-03-04 16:18:39 -0500

    refactored equal definition. unique WIP

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -3,14 +3,11 @@ function equal(first, second) {
   if (!Array.isArray(first) || !Array.isArray(second)) {
     return { error: 'Invalid arguments passed. First and second argument must be an array' };
   }
-  if (first.length !== second.length) {
-    return false;
-  }
   let result = true;
   for (const i in first) {
     if (first[i] !== second[i]) {
       result = false;
     }
   }
-  return result;
+  return first.length !== second.length ? false : result;
 }
--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,16 @@
 /* exported unique */
+/*
+  define a function with one para, array
+  if argument passed is not an array, return error
+  assign an empty array to a variable result
+  declare a let variable
+  intitialize a for.. in loop
+  assign the let variable to the value of array[i]
+  use an expression to determine if we are pushing the value to result array
+  return result
+  i dont know what im doing
+ */
+
+function unique(array) {
+  return 'lel';
+}
