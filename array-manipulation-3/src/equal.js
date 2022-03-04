f11d4d9a46ff8e55a25880da52c2d9414d22e346
Owen Adams <owenadams.main@gmail.com>
2022-03-04 09:31:29 -0500

    complete equal function definition with passing test cases

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,1 +1,16 @@
 /* exported equal */
+function equal(first, second) {
+  if (!Array.isArray(first) || !Array.isArray(second)) {
+    return { error: 'Invalid arguments passed. First and second argument must be an array' };
+  }
+  if (first.length !== second.length) {
+    return false;
+  }
+  let result = true;
+  for (const i in first) {
+    if (first[i] !== second[i]) {
+      result = false;
+    }
+  }
+  return result;
+}
