a78afc814c7e4990161a52b6b6bd0c44455d4068
Owen Adams <owenadams.main@gmail.com>
2022-03-06 10:55:42 -0500

    ignore

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -5,10 +5,19 @@ function titleCase(string) {
   }
   const lwrArray = string.toLowerCase().split(' ');
   const result = [];
+  const exceptions = ['The', 'Of', 'And', 'In', 'To', 'On', 'For'];
   for (let i = 0; i < lwrArray.length; i++) {
-    result.push(lwrArray[i].charAt(0).toUpperCase() + lwrArray[i].slice(1).toLowerCase());
-    if (result[i] === 'The' || result[i] === 'Of' || result[i] === 'And' || result[i] === 'In' || result[i] === 'To' || result[i] === 'On' || result[i] === 'For') {
-      result[i] = result[i].charAt(0).toLowerCase() + result[i].slice(1);
+    result.push(lwrArray[i].charAt(0).toUpperCase() +
+      lwrArray[i].slice(1).toLowerCase());
+    if (i !== 0 && exceptions.includes(result[i]) && !lwrArray[i - 1].includes(':')) {
+      result[i] = result[i].charAt(0).toLowerCase() +
+        result[i].slice(1);
+    }
+    if (result[i].includes('-')) {
+      const posDash = result[i].indexOf('-');
+      result[i] = result[i].slice(0, posDash + 1) +
+        result[i].charAt(posDash + 1).toUpperCase() +
+        result[i].slice(posDash + 2);
     }
     if (result[i] === 'Javascript') {
       result[i] = 'JavaScript';
