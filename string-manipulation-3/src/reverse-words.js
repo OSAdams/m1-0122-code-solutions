3b2461d2895eb3f1af8ccd61873b5a4bb939d80c
Owen Adams <owenadams.main@gmail.com>
2022-03-06 12:14:47 -0500

    complete reverseWord function definition with passing test cases

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,15 @@
 /* exported reverseWords */
+function reverseWords(string) {
+  if (typeof string !== 'string') {
+    return { error: 'Invalid argument passed. Argument must be a string' };
+  }
+  const holder = string.split(' ');
+  const result = [];
+  for (const i in holder) {
+    const temp = holder[i].split('').reverse().join('');
+    result.push(temp);
+  }
+  return result.join(' ');
+}
+
+// remove
