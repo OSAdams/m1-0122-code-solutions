a5db73d71fec68c00db9ab25b5ad84d08254f452
Owen Adams <owenadams.main@gmail.com>
2022-03-07 16:04:00 -0500

    Refactor deck function definition

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -6,33 +6,21 @@ function BuildCards(value, score, suit) {
 }
 
 function deck() {
-  this.score = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-  this.suit = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
+  const value = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  const suit = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
+  let addToIndex = 2;
+  const kOrA = ['K', 'A'];
   const fullDeck = [];
-  for (const i in this.suit) {
-    for (let j = 0; j < this.score.length; j++) {
-      let letterValue = 0;
-      // if (score[j] === 'J') {
-      //   letterValue = 1;
-      // } else if (score[j] === 'Q') {
-      //   letterValue = 2;
-      // } else if (score[j] === 'K') {
-      //   letterValue = 3;
-      // } else if (score[j] === 'A') {
-      //   letterValue = 4;
-      // } else {
-      //   letterValue = 0;
-      // }
-      this.score[j] === 'J'
-        ? letterValue = 1
-        : this.score[j] === 'Q'
-          ? letterValue = 2
-          : this.score[j] === 'K'
-            ? letterValue = 3
-            : this.score[j] === 'A'
-              ? letterValue = 4
-              : letterValue = 0;
-      fullDeck.push(new BuildCards(j + 2 + letterValue, this.score[j], this.suit[i]));
+  for (const i in suit) {
+    for (let j = 0; j < value.length; j++) {
+      kOrA.includes(value[j]) // instead of assining an array to a variable, i can use score.slice(-2).includes(score[j]) - which is better?
+        ? addToIndex = -1
+        : value[j] === 'J'
+          ? addToIndex = 1
+          : value[j] === 'Q'
+            ? addToIndex = 0
+            : addToIndex = 2;
+      fullDeck.push(new BuildCards(parseInt(value[j], j + addToIndex), suit[i]));
     }
   }
   return fullDeck;
