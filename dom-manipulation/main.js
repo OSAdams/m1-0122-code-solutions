e91badbd46af4224a6fc18413793c05b71340f49
Owen Adams <owenadams.main@gmail.com>
2022-03-08 22:44:04 -0500

    add value to variable. use ternary operator to set className when span textContent hits a certain number.

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -1,7 +1,20 @@
 const $clickCounter = document.querySelector('.click-number');
 const $hotButton = document.querySelector('.hot-button');
+
+const hButton = 'hot-button ';
 $hotButton.addEventListener('click', e => {
   $clickCounter.textContent++;
-  // eslint-disable-next-line
-  console.log($clickCounter.textContent)
+  $clickCounter.textContent < 4
+    ? $hotButton.className = hButton + ' cold'
+    : $clickCounter.textContent < 7
+      ? $hotButton.className = hButton + ' cool'
+      : $clickCounter.textContent < 10
+        ? $hotButton.className = hButton + ' tepid'
+        : $clickCounter.textContent < 13
+          ? $hotButton.className = hButton + ' warm'
+          : $clickCounter.textContent < 16
+            ? $hotButton.className = hButton + ' hot'
+            : $hotButton.className = hButton + ' nuclear';
 });
+
+// comment to remove
