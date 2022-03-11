59e1886e95d6f36bed0ef728a6bf72e505460ec8
Owen Adams <owenadams.main@gmail.com>
2022-03-10 23:40:09 -0500

    add styling query the document add event listener. ready for submit

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,2 +1,12 @@
-// eslint-disable-next-line
-console.log('hello world');
+const $container = document.querySelector('.container');
+const $circle = document.querySelector('.circle');
+
+$circle.addEventListener('click', e => {
+  if ($circle.className === 'circle off') {
+    $circle.className = 'circle';
+    $container.style.background = 'rgb(248 233 215)';
+  } else {
+    $circle.className += ' off';
+    $container.style.background = 'rgb(40 40 40)';
+  }
+});
