072a5d6a4ad433d25d6c9872f40cafb2bed80d7f
Owen Adams <owenadams.main@gmail.com>
2022-03-11 21:11:28 -0500

    query dom for elements, define function to set className of dom elements, add eventListener for clicks

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,2 +1,12 @@
-// eslint-disable-next-line
 const $openModal = document.querySelector('.open-modal');
+const $closeModal = document.querySelector('.close-modal');
+const $container = document.querySelectorAll('.container');
+
+function onClick(e) {
+  e.target === $openModal
+    ? $container[1].className = 'container'
+    : $container[1].className += ' hidden';
+}
+
+$openModal.addEventListener('click', onClick);
+$closeModal.addEventListener('click', onClick);
