960ccb9a757ca671fdfb6f526e83e86132466d62
Owen Adams <owenadams.main@gmail.com>
2022-03-12 22:59:36 -0500

    add styling, add functionality and new event listener for modal pop up. add div containig modal. WIP

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,14 +1,18 @@
 const $text = document.querySelectorAll('.letter');
+const $complete = document.querySelector('.complete');
+const $playAgain = document.querySelector('.play-again');
 
 document.addEventListener('keydown', keydownTracker);
+$playAgain.addEventListener('click', reset);
 
 let textCounter = 0;
+
 function keydownTracker(event) {
   if ($text[textCounter]) {
     $text[textCounter].style.borderBottom = 'thick solid #020202';
   }
   if (event.key === $text[textCounter].textContent) {
-    $text[textCounter].style.color = 'green';
+    $text[textCounter].style.color = 'limegreen';
     textCounter++;
     if ($text[textCounter]) {
       $text[textCounter].style.borderBottom = 'thick solid #020202';
@@ -19,8 +23,15 @@ function keydownTracker(event) {
     $text[textCounter].style.borderBottom = 'thick solid red';
   }
 
-  if ($text[$text.length - 1].style.color === 'green') {
-    // eslint-disable-next-line no-console
-    console.log('Congrats');
+  if ($text[$text.length - 1].style.color === 'limegreen') {
+    $complete.className = 'complete';
   }
+}
+
+function reset(event) {
+  $complete.className += ' hidden';
+  for (let i = 0; i < $text.length; i++) {
+    $text[i].style.color = '#020202';
+  }
+  textCounter = 0;
 }
