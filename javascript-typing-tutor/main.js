5e4eae7644fac98cb8446da146a6aff86fc30009
Owen Adams <owenadams.main@gmail.com>
2022-03-12 22:11:57 -0500

    added functionality to track keydown events and update the dom to match tutorial. WIP

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -4,10 +4,23 @@ document.addEventListener('keydown', keydownTracker);
 
 let textCounter = 0;
 function keydownTracker(event) {
+  if ($text[textCounter]) {
+    $text[textCounter].style.borderBottom = 'thick solid #020202';
+  }
   if (event.key === $text[textCounter].textContent) {
     $text[textCounter].style.color = 'green';
     textCounter++;
+    if ($text[textCounter]) {
+      $text[textCounter].style.borderBottom = 'thick solid #020202';
+    }
+    $text[textCounter - 1].style.borderBottom = 'none';
   } else {
     $text[textCounter].style.color = 'red';
+    $text[textCounter].style.borderBottom = 'thick solid red';
+  }
+
+  if ($text[$text.length - 1].style.color === 'green') {
+    // eslint-disable-next-line no-console
+    console.log('Congrats');
   }
 }
