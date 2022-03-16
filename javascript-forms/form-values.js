1fe7fb80294127b107615b9951f08be07108cea0
Owen Adams <owenadams.main@gmail.com>
2022-03-15 22:42:21 -0400

    query dom for contact-form id. addEventListener for submit, prevent reload, assign input values to properties in an object. console log object.

---
+++ javascript-forms/form-values.js
@@ -0,0 +1,11 @@
+const $contactForm = document.querySelector('#contact-form');
+
+$contactForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const messageData = {
+    name: $contactForm.name.value,
+    email: $contactForm.email.value,
+    message: $contactForm.message.value
+  };
+  console.log(messageData);
+});
