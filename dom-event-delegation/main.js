1c843254867b5665d5930d1a7d86cac9e7f24fb8
Owen Adams <owenadams.main@gmail.com>
2022-03-21 22:27:26 -0400

    add conditional, declare variable with e.target.closest value, console log value, remove value from dom

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -3,4 +3,11 @@ const $taskList = document.querySelector('.task-list');
 $taskList.addEventListener('click', e => {
   console.log('event.target: ', e.target);
   console.log('event.target.tagName: ', e.target.tagName);
+  if (e.target.tagName === 'BUTTON') {
+    const closestToEventTarget = e.target.closest('.task-list-item');
+    console.log('e.target.closest: ', closestToEventTarget);
+    closestToEventTarget.remove();
+  }
 });
+
+// comment to remove
