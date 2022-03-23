d528f966696d587cc7df811ee869807f3e1c1efe
Owen Adams <owenadams.main@gmail.com>
2022-03-22 22:47:47 -0400

    remove arrow function, define new function, add function into event listener callback. get attribute, use conditional expression to update dom element classnames

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -1,14 +1,22 @@
 const $tabContainer = document.querySelector('.tab-container');
 const $tabs = document.querySelectorAll('.tab');
 // eslint-disable-next-line
-const $views = document.querySelectorAll('.views');
+const $views = document.querySelectorAll('.view');
 
-$tabContainer.addEventListener('click', event => {
+$tabContainer.addEventListener('click', swapView);
+
+function swapView(event) {
   if (event.target.matches('.tab')) {
     for (let i = 0; i < $tabs.length; i++) {
       event.target === $tabs[i]
         ? $tabs[i].className += ' active'
         : $tabs[i].className = 'tab';
     }
+    const eTargetDataView = event.target.getAttribute('data-view');
+    for (let i = 0; i < $views.length; i++) {
+      $views[i].getAttribute('data-view') === eTargetDataView
+        ? $views[i].className = 'view'
+        : $views[i].className = 'view hidden';
+    }
   }
-});
+}
